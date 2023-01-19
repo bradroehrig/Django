0f53aa3153f6340a11ff863410158d542d8bf520
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-01-19 09:46:17 -0600

    commit

--- tutorial/first/demo/models.py
+++ tutorial/first/demo/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
-# Create your models here.
-=======
-class Book(models.Model):
-    title = models.CharField(max_length=36)
->>>>>>> f9532b1245e29682ec19606885dea96d84f00993
