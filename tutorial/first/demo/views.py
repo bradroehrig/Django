f9532b1245e29682ec19606885dea96d84f00993
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-01-18 20:18:55 -0600

    commit

--- tutorial/first/demo/views.py
+++ tutorial/first/demo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
+class Another(View):
+    def get(self, request):
+        return HttpResponse('This is another function inside class')
+
 
 def first(request):
     return HttpResponse('First message fropm views')
--- tutorial/first/first/urls.py
+++ tutorial/first/first/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('demo/', include('demo.urls')),
-    path('/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
