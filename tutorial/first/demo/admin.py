04e2b9ca837140b9c15be126ac016197ce17d061
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-01-20 12:46:35 -0600

    commit

--- tutorial/first/demo/admin.py
+++ tutorial/first/demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, BookNumber
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -7,3 +7,4 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ['published']
     search_fields = ['title', 'description']
 
+admin.site.register(BookNumber)
--- tutorial/first/demo/models.py
+++ tutorial/first/demo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class BookNumber(models.Model):
+    isbn_10 = models.CharField(max_length=10, blank=True)
+    isbn_13 = models.CharField(max_length=10, blank=True)
 class Book(models.Model):
     title = models.CharField(max_length=36, blank=False, unique=True)
     description = models.TextField(max_length=256, blank=True)
@@ -9,5 +11,8 @@ class Book(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='covers/', blank=True)
     
+    number = models.OneToOneField(BookNumber, null=True, blank=True, on_delete=models.CASCADE)
+        
     def __str__ (self):
         return self.title
+    
